4fa42d23de77e6700f8639b4db03329e272125c7
Axel <axelramirezludewig@gmail.com>
2022-11-07 12:30:46 -0600

    changes

--- src/server/trpc/router/itemRouter.ts
+++ src/server/trpc/router/itemRouter.ts
@@ -2,18 +2,26 @@ import * as trpc from '@trpc/server';
 import { any, z } from 'zod';
 import { publicProcedure, router } from '../trpc';
 import { Prisma } from '@prisma/client';
-import { client }
 
-const itemRouter = router({
-    addItem: publicProcedure
-      .input(z.string())
-      .mutation(async ({ input }) => {
-        const post = await client.post.create({
-          data: input,
+
+export const itemRouter = router({
+  add: publicProcedure
+    .input(z.object({ text: z.string().nullish() }).nullish())
+    .mutation(async ({ input, ctx }) => { 
+         try {
+            await ctx.prisma.items.create({
+          data: {
+            name: input?.text
+          },
         });
-        return post;
-      }),
-  });   
-
+      } catch (error) {
+        console.log(error);
+      }
+    }),
+  getAll: publicProcedure.query(({ ctx }) => {
+    return ctx.prisma.items.findMany();
+    
+  }),
+});
 
 export type itemRouter = typeof itemRouter;
